b68844f3e3d4e65f8be349c9122e14f98a746062
Agiratech-Madhan <madhan.k@agiratech.com>
2023-04-17 11:14:31 +0530

    removed unwanted files

--- movieapp_riverpod/movie_app_riverpod/lib/constants.dart
+++ movieapp_riverpod/movie_app_riverpod/lib/constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class Constants {
-  static String apiKey = 'b6ee00729c9ca9a458e3625f96877b1a';
   static String userProfile =
       'https://image.shutterstock.com/mosaic_250/287881/1768126784/stock-photo-young-handsome-man-with-beard-wearing-casual-sweater-and-glasses-over-blue-background-happy-face-1768126784.jpg';
 
